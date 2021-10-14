0d8e12b16824954b00c918d9376a42bcee664c17
최주성 <62999641+choi9923@users.noreply.github.com>
2021-10-14 14:13:53 +0900

    2021-10-14
    botton,Animation

---
+++ Meataverse/Assets/Script/MainUiManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainUiManager : MonoBehaviour
+{
+
+    public void StartBtn()
+    {
+        SceneManager.LoadScene("SampleScene");
+        Debug.Log("스타또!");
+    }
+    public void LoadBtn()
+    {
+        Debug.Log("로드...");
+    }
+    public void QuitBtn()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        
+
+    }
+}
---
+++ Meataverse/Assets/Script/UiManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+    static public UiManager instance;
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public void QuitBtn()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
+
+    }
+}
