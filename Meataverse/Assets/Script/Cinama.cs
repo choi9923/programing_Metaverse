3544b927f7b814a947dd06b463a139f9fd7cd7fb
최주성 <62999641+choi9923@users.noreply.github.com>
2021-10-21 12:24:41 +0900

    2021-10-21 Animation, Camere

---
+++ Meataverse/Assets/Script/Cinama.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Cinama : MonoBehaviour
+{
+    public GameObject tPlayer; public Transform tFollowTarget; 
+    private CinemachineVirtualCamera vcam; 
+    void Start() { vcam = GetComponent<CinemachineVirtualCamera>(); tPlayer = null; } 
+    // 밑 업데이트가 아닌 start에서 한번만 실행해도 되는게 아닐까?
+    void Update(){ 
+        if (tPlayer == null)
+        { 
+            tPlayer = GameObject.FindWithTag("Player");
+        if (tPlayer != null)
+            { 
+                tFollowTarget = tPlayer.transform; vcam.Follow = tFollowTarget;
+            } 
+        } 
+    }
+
+    
+}
--- Meataverse/Assets/Script/PlayerControll.cs
+++ Meataverse/Assets/Script/PlayerControll.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class PlayerControll : MonoBehaviour
 {
+    Animator pAnimator;
+
     static public PlayerControll instance;
     public Vector3 dir;
     public float speed = 10;
     // Start is called before the first frame update
+
+    enum States
+    {
+        righst = 1,
+        left = 2,
+        up = 3,
+        down = 4,
+        idle = 5
+            
+    }
     void Start()
     {
+        pAnimator = GetComponent<Animator>();
         if(instance == null)
         {
             instance = this;
@@ -33,12 +46,46 @@ public class PlayerControll : MonoBehaviour
     }
     void keyInput()
     {
-        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.x = Input.GetAxisRaw("Horizontal");       
         dir.y = Input.GetAxisRaw("Vertical");
+        if (dir.x != 0 || dir.y != 0)
+        {
+            UpdateState();
+        }
+        else
+        {
+            pAnimator.SetLayerWeight(1, 1);
+        }
+        
     }
     private void Move()
     {      
         transform.position += dir * speed * Time.deltaTime;
 
     }
-}
+
+    private void UpdateState()
+    {
+        pAnimator.SetLayerWeight(1, 0);
+        if (dir.x > 0)
+        {
+            pAnimator.SetInteger("MoveDir", (int)States.righst);
+        }
+        else if (dir.x < 0)
+        {
+            pAnimator.SetInteger("MoveDir", (int)States.left);
+
+        }
+        else if (dir.y > 0)
+        {
+
+            pAnimator.SetInteger("MoveDir", (int)States.up);
+        }
+        else if (dir.y < 0)
+        {
+            pAnimator.SetInteger("MoveDir", (int)States.down);
+        }
+        
+    }
+
+    }
